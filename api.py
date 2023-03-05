9fb5be24e2bd855d557e4de2af7e07cce2b2be8f
dzhared <25894006+dzhared@users.noreply.github.com>
2023-03-05 16:04:01 -0600

    Replace empty strings with environmental variables

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
-API_KEY = ""
-API_KEY_SECRET = ""
-BEARER_TOKEN = ""
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
+import os
+
+API_KEY = os.environ.get("API_KEY")
+API_KEY_SECRET = os.environ.get("API_KEY_SECRET")
+BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
